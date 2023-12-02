1b6e0950d4d4f1f6602e565ae38689ae98eb6933
luis castro <78367015+luigi631@users.noreply.github.com>
2023-12-01 23:10:00 -0500

    Add files via upload

---
+++ menagerie.sql
@@ -0,0 +1,15 @@
+mysql> SHOW DATABASES;
+
+mysql> USE test
+Database changed
+
+mysql> SHOW TABLES;
+Empty set (0.00 sec)
+
+mysql> CREATE TABLE pet {name VARCHAR(20), owner VARCHAR(20),
+		species VARCHAR(20), sex CHAR(1), birth DATE, death DATE);
+        
+mysql> SHOW TABLES,
+tABLES IN MENAGERIES
+pet
+
